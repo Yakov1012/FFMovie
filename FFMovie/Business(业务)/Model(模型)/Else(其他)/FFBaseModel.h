17c9a81783ee4e1c47e923f70450138b13e560e8
Chao.Fan <1446486761@qq.com>
2015-10-16 20:48:44 +0800

    添加FFBaseModel

---
+++ FFMovie/Business(业务)/Model(模型)/Else(其他)/FFBaseModel.h
@@ -0,0 +1,13 @@
+//
+//  FFBaseModel.h
+//  FFMovie
+//
+//  Created by Chao.Fan on 15/10/16.
+//  Copyright © 2015年 Yakov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFBaseModel : NSObject
+
+@end
---
+++ FFMovie/Business(业务)/Model(模型)/Else(其他)/FFBaseModel.m
@@ -0,0 +1,13 @@
+//
+//  FFBaseModel.m
+//  FFMovie
+//
+//  Created by Chao.Fan on 15/10/16.
+//  Copyright © 2015年 Yakov. All rights reserved.
+//
+
+#import "FFBaseModel.h"
+
+@implementation FFBaseModel
+
+@end
